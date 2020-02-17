12e27bd62fed57a863263d9b87a683ad2952c46d
Leigh <leightheduck@gmail.com>
2020-02-17 15:00:03 +1100

    Added passthrough testing for dual CAN interface setup.

--- esp8266_direct_pca82c250_sw_serial/src/main.cpp
+++ esp8266_direct_pca82c250_sw_serial/src/main.cpp
@@ -1,21 +1,53 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial swSer(D5,D6); // RX, TX
+SoftwareSerial sidSerial(D5,D6); // RX, TX
+SoftwareSerial icmSerial(D7,D8); // RX, TX
 #define sw_ser_baud 57600
+
+bool passthrough = true;
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println("Start PCA82C250 Software Serial Monitor");  
-  swSer.begin(sw_ser_baud);
+
+  if(passthrough)
+  {
+    Serial.println("PASSTHROUGH ENABLED");
+  }
+
+  sidSerial.begin(sw_ser_baud);
+  icmSerial.begin(sw_ser_baud);
   delay(2000);
 }
 
 void loop()
 {
-  while(swSer.available() > 0)
+  while(sidSerial.available() > 0)
   {
-    Serial.write(swSer.read());
+    char inbound = sidSerial.read();
+
+    Serial.print("SID: ");
+    Serial.write(inbound);
+    Serial.println("");
+
+    if(passthrough)
+    {
+      icmSerial.write(inbound);
+    }
+  }
+  while(icmSerial.available() > 0)
+  {
+    char inbound = icmSerial.read();
+
+    Serial.print("ICM: ");
+    Serial.write(inbound);
+    Serial.println("");
+
+    if(passthrough)
+    {
+      sidSerial.write(inbound);
+    }
   }
 }
