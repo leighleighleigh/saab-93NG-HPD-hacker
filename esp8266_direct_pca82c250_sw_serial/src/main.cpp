d5c6e08b6f57e2750346e99678a7665975715f0e
Leigh <leightheduck@gmail.com>
2020-02-18 08:18:39 +1100

    Revised pinouts for perfboard proto

--- esp8266_direct_pca82c250_sw_serial/src/main.cpp
+++ esp8266_direct_pca82c250_sw_serial/src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial sidSerial(D7,D8); // RX, TX
-SoftwareSerial icmSerial(D5,D6); // RX, TX
+SoftwareSerial sidSerial(D6, D7); // RX, TX
+SoftwareSerial icmSerial(D4, D5); // RX, TX
 #define sw_ser_baud 56000
 
 bool passthroughICMTOSID = true;
@@ -12,12 +12,14 @@ bool showTX = false;
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("Start PCA82C250 Software Serial Monitor");  
+  Serial.println("Start PCA82C250 Software Serial Monitor");
 
   sidSerial.begin(sw_ser_baud);
   icmSerial.begin(sw_ser_baud);
 
   delay(2000);
+
+  Serial.println("START!");
 }
 
 char inbound = ' ';
@@ -25,44 +27,45 @@ char inbound = ' ';
 void loop()
 {
   // sidSerial.listen();
-  if(sidSerial.available())
+  if (sidSerial.available())
   {
     inbound = sidSerial.read();
 
     Serial.print("SID: ");
-    Serial.print(inbound,HEX);
+    Serial.print(inbound, HEX);
     Serial.println("");
 
-    if(passthroughSIDTOICM)
+    if (passthroughSIDTOICM)
     {
       icmSerial.stopListening();
       icmSerial.write(inbound);
-      if(showTX){
-      Serial.print(">>>ICM: ");
-      Serial.print(inbound,HEX);
-      Serial.println("");
+      if (showTX)
+      {
+        Serial.print(">>>ICM: ");
+        Serial.print(inbound, HEX);
+        Serial.println("");
       }
       icmSerial.listen();
     }
   }
 
   // icmSerial.listen();
-  if(icmSerial.available())
+  if (icmSerial.available())
   {
     inbound = icmSerial.read();
 
     Serial.print("ICM: ");
-    Serial.print(inbound,HEX);
+    Serial.print(inbound, HEX);
     Serial.println("");
 
-    if(passthroughICMTOSID)
+    if (passthroughICMTOSID)
     {
       sidSerial.stopListening();
-      if(showTX)
+      if (showTX)
       {
-      Serial.print(">>>SID: ");
-      Serial.print(inbound,HEX);
-      Serial.println("");
+        Serial.print(">>>SID: ");
+        Serial.print(inbound, HEX);
+        Serial.println("");
       }
       sidSerial.write(inbound);
       sidSerial.listen();
