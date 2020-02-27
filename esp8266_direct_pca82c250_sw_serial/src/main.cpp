9500ba413334b9f8d831313f8099025ca2e5ffcf
Leigh <leightheduck@gmail.com>
2020-02-27 11:19:35 +1100

    Loopback test mode complete. Working on buffer-then-send main function.

--- esp8266_direct_pca82c250_sw_serial/src/main.cpp
+++ esp8266_direct_pca82c250_sw_serial/src/main.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
+#include <math.h>
 
 SoftwareSerial sidSerial(D5, D6); // RX, TX
 SoftwareSerial icmSerial(D2, D3); // RX, TX
 #define sw_ser_baud 56000
 
-bool testMode = true; // In test mode, the TX pins are set high.
+bool testMode = false; // In test mode, the TX pins are set high.
 bool passthroughICMTOSID = true;
 bool passthroughSIDTOICM = true;
-bool showTX = false;
 
 void setup()
 {
@@ -25,78 +25,59 @@ void setup()
   {
     Serial.println("TEST MODE ENGAGED");
   }
+
+  icmSerial.flush();
+  sidSerial.flush();
 }
 
 char inbound = ' ';
 
 void loop()
 {
-  // delay(50);
   if (!testMode)
   {
-    // sidSerial.listen();
-    while (sidSerial.available())
-    {
-      inbound = sidSerial.read();
-
-      Serial.print("  SID: ");
-      Serial.print(inbound, HEX);
-      Serial.println("");
-
-      if (passthroughSIDTOICM)
-      {
-        if (inbound != 0)
-        {
-          // If it's a zero, don't send
-          icmSerial.stopListening();
-          icmSerial.write(inbound);
-          if (showTX)
-          {
-            Serial.print(">>>ICM: ");
-            Serial.print(inbound, HEX);
-            Serial.println("");
-          }
-          icmSerial.listen();
-        }
-      }
-    }
-
-    // icmSerial.listen();
     while (icmSerial.available())
     {
       inbound = icmSerial.read();
 
-      Serial.print("ICM: ");
+      Serial.print("ICMrx: ");
       Serial.print(inbound, HEX);
       Serial.println("");
 
       if (passthroughICMTOSID)
       {
-        if (inbound != 0)
-        {
           sidSerial.stopListening();
-          if (showTX)
-          {
-            Serial.print(">>>SID: ");
-            Serial.print(inbound, HEX);
-            Serial.println("");
-          }
           sidSerial.write(inbound);
           sidSerial.listen();
-        }
+      }
+    }
+    while (sidSerial.available())
+    {
+      inbound = sidSerial.read();
+
+      Serial.print("SIDrx: ");
+      Serial.print(inbound, HEX);
+      Serial.println("");
+
+      if (passthroughSIDTOICM)
+      {
+        icmSerial.stopListening();
+        icmSerial.write(inbound);
+        icmSerial.listen();
       }
     }
   }
   else
   {
-    delay(3000);
+    delay(1000);
 
     // ICM -> SID
     // Clear buffers
     icmSerial.flush();
     sidSerial.flush();
     Serial.println(">ICM tx");
-    icmSerial.println("hello sid!");
+    // icmSerial.println("hello sid!");
+    icmSerial.println("testtinjaowijdoiawjtesttinjaowijdoiawj!");
     delay(20);
 
     Serial.println("SID rx");
@@ -107,7 +88,7 @@ void loop()
       Serial.write(inbound);
     }
 
-    delay(3000);
+    delay(1000);
 
     // SID -> ICM
     // Clear buffers
