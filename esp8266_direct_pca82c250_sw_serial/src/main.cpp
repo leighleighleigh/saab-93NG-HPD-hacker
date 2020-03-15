e75bb1b39ddfbf83fb5802d67e8bcb74b1f6ec6c
Leigh <leightheduck@gmail.com>
2020-03-15 21:22:41 +1100

    Working on software serial approach again

--- esp8266_direct_pca82c250_sw_serial/src/main.cpp
+++ esp8266_direct_pca82c250_sw_serial/src/main.cpp
@@ -2,97 +2,98 @@
 #include <SoftwareSerial.h>
 #include <math.h>
 
-
-SoftwareSerial sidSerial(D5, D6); // RX, TX
-SoftwareSerial icmSerial(D2, D3); // RX, TX
-#define sw_ser_baud 56000 // This is a known working BAUD rate for the DLA UART decoder.
-
-bool testMode = false; // In test mode, the TX pins are set high.
+SoftwareSerial sidSerial(D2, D3); // RX, TX
+SoftwareSerial icmSerial(D5, D6); // RX, TX
+#define sw_ser_baud 56000         // This is a known working BAUD rate for the DLA UART decoder.
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("Start PCA82C250 Software Serial Monitor");
+  Serial.println("START!");
 
+  // Begin the software serials
   sidSerial.begin(sw_ser_baud);
   icmSerial.begin(sw_ser_baud);
 
-  delay(2000);
+  // Listen on icmSerial first
+  icmSerial.listen();
+  sidSerial.stopListening();
 
-  Serial.println("START!");
-  if (testMode)
-  {
-    Serial.println("TEST MODE ENGAGED");
-  }
-
+  // Flush buffers
   icmSerial.flush();
   sidSerial.flush();
 }
 
 char inbound = ' ';
+uint8_t inboundCount = 0;
+char busState = 'I';
 
 void loop()
 {
-  if (!testMode)
+  if (busState == 'I')
   {
-    if (icmSerial.available()) {      // If anything comes in Serial (USB),
+    // Stop listening to sidSerial while we send on it
+    sidSerial.stopListening();
+
+    // Get all the icmSerial stuff
+    Serial.print("ICM rx: ");
+    while (icmSerial.available())
+    {
+      // Read from ICM
       inbound = icmSerial.read();
-      // Stop listening to SID
-      // sidSerial.stopListening();
-      // sidSerial.write(inbound);   // read it and send it out Serial1 (pins 0 & 1)
-      Serial.print("ICM: ");
-      Serial.println(inbound, HEX);
+      // Write out to SID
+      sidSerial.write(inbound);
+      // Increment inboundCOUnt
+      inboundCount++;
+      // Debug that
+      Serial.print(inbound, HEX);
     }
-    // sidSerial.listen();
+    // Newline
+    Serial.println("");
 
-    if (sidSerial.available()) {     // If anything comes in Serial1 (pins 0 & 1)
-      inbound = sidSerial.read();
-      // icmSerial.stopListening();
-      // icmSerial.write(inbound);   // read it and send it out Serial (USB)
-      Serial.print("SID: ");
-      Serial.println(inbound, HEX);
+    // Re-listen on SID
+    sidSerial.listen();
+
+    // If we have printed more than 2 chars, then we can go look for a reply!
+    if(inboundCount >= 2)
+    {
+      // Change busstate
+      busState = 'S';
+      // Reset inboundCount
+      inboundCount = 0;
     }
-    // icmSerial.listen();
-
   }
   else
   {
-    delay(1000);
+    // Stop listening to sidSerial while we send on it
+    icmSerial.stopListening();
 
-    // ICM -> SID
-    // Clear buffers
-    icmSerial.flush();
-    sidSerial.flush();
-    Serial.println(">ICM tx");
-    Serial.println("!abcdefghijklmnopqrstuvwxyz!123456789090!");
-    icmSerial.println("!abcdefghijklmnopqrstuvwxyz!123456789090!");
-    delay(50);
-
-    Serial.println("SID rx");
-    // Print on ICM, read from SID
+    // Get all the icmSerial stuff
+    Serial.print("SID rx: ");
     while (sidSerial.available())
     {
+      // Read from SID
       inbound = sidSerial.read();
-      Serial.write(inbound);
+      // Write out to ICM
+      icmSerial.write(inbound);
+      // Increment inboundCOUnt
+      inboundCount++;
+      // Debug that
+      Serial.print(inbound, HEX);
     }
+    // Newline
+    Serial.println("");
 
-    delay(1000);
+    // Re-listen on ICM
+    icmSerial.listen();
 
-    // SID -> ICM
-    // Clear buffers
-    icmSerial.flush();
-    sidSerial.flush();
-    Serial.println(">SID tx");
-    Serial.println("hello icm!");
-    sidSerial.println("hello icm!");
-    delay(20);
-
-    Serial.println("ICM rx");
-    // Print on ICM, read from SID
-    while (icmSerial.available())
+    // If we have printed more than 2 chars, then we can go look for a reply!
+    if(inboundCount >= 2)
     {
-      inbound = icmSerial.read();
-      Serial.write(inbound);
+      // Change busstate
+      busState = 'I';
+      // Reset inboundCount
+      inboundCount = 0;
     }
   }
 }
