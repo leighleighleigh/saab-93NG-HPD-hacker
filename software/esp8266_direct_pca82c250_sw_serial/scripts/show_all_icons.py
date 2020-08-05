a3fbab72f9dd36f1aec3649567c3f8bb35ddb04f
Leigh Oliver <leighcharlesoliver@gmail.com>
2020-08-05 19:58:21 +1000

    Messing with icons

--- software/esp8266_direct_pca82c250_sw_serial/scripts/show_all_icons.py
+++ software/esp8266_direct_pca82c250_sw_serial/scripts/show_all_icons.py
@@ -9,6 +9,7 @@ rather than having to hard-code them into the arduino, or use other (clunky) web
 import asyncio
 import websockets
 import time
+import math
 
 iconVal = 64
 
@@ -27,29 +28,32 @@ def convertTXT(msg):
     return ",".join(convert)
 
 async def hello():
+    testvalue = 0
     uri = "ws://10.0.0.113:81"
     async with websockets.connect(uri) as websocket:
         await websocket.send(backlightCMD)
         time.sleep(0.05)
-        await websocket.send(clearCMD)
-        time.sleep(0.05)
         await websocket.send(clearTXT)
         time.sleep(0.05)
-        
-        await websocket.send(iconCMD.format(hex(iconVal+1)))
-        time.sleep(0.05)
-        
 
-        await websocket.send(iconCMD.format(hex(iconVal)))
-        time.sleep(0.05)
-
-        await websocket.send(drawCMD)
-        time.sleep(2)
-        #await websocket.send(textFMT.format(hex(138),hex(4),convertTXT("ICON NO." + str(iconVal))))
-        
-        #time.sleep(2)
-        #await websocket.send(drawTXT)
-        #time.sleep(2)
-        
+        while testvalue < 1000:
+            await websocket.send(clearTXT)
+            time.sleep(0.05)
+            await websocket.send(textFMT.format(hex(138 + int(abs(math.sin(testvalue/50)*30))),hex(int(abs(math.sin(testvalue/50)*20))),convertTXT("TEST VAL " + str(testvalue))))
+            time.sleep(0.05)
+            await websocket.send(drawTXT)
+            time.sleep(0.05)
+            #await websocket.send("0x81,0x00")
+            #time.sleep(0.05)
+            testvalue += 1
+            # await websocket.send(clearCMD)
+            # time.sleep(0.05)
+            # await websocket.send(iconCMD.format(hex(iconVal+1)))
+            # time.sleep(0.05)
+            # await websocket.send(iconCMD.format(hex(iconVal)))
+            # time.sleep(0.05)
+            # await websocket.send(drawCMD)
+            # time.sleep(2)
+            
         
 asyncio.get_event_loop().run_until_complete(hello())
